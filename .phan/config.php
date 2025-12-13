ddfc8a3a412ade1f6091377b26422cd60d1ca083
Michael Moll <kvedulv@kvedulv.de>
2025-12-13 17:19:22 +0100

    phan

--- .phan/config.php
+++ .phan/config.php
@@ -22,8 +22,7 @@ return [
     'directory_list' => [
         'MO4',
         'tests',
-        'vendor/squizlabs/php_codesniffer',
-        'vendor/symfony/polyfill-php83',
+        'vendor'
     ],
 
     // A directory list that defines files that will be excluded
